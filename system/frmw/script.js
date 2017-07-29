faa67198a70c1ba8057598f782e702a20310cf89
Loquicom <Loquicom@users.noreply.github.com>
2017-07-29 13:25:27 +0200

    Refonte de la pagination (s'adapte au nombre de page total)

--- system/frmw/script.js
+++ system/frmw/script.js
@@ -23,7 +23,121 @@ $(document).ready(function () {
         }
     });
 
+    //Pagination
+    $(document).on('click', '.pagine-num', function () {
+        _change_page_pagine($(this).attr('data-num'));
+    });
+    $(document).on('click', '#pagine-prev', function () {
+        if (!$(this).hasClass('disabled')) {
+            _change_page_pagine(_paginePageActuel - 1);
+        }
+    });
+    $(document).on('click', '#pagine-next', function () {
+        if (!$(this).hasClass('disabled')) {
+            _change_page_pagine(_paginePageActuel + 1);
+        }
+    });
+
 });
+
+var _paginePageActuel = 1;
+var _pagineElement = null;
+var _pagineNbPage = 0;
+var _pagineCallback = null;
+function pagine(nbPage, element, callback = function(page) {}) {
+    _pagineElement = '#' + element;
+    _pagineNbPage = nbPage;
+    _pagineCallback = callback;
+    var html = '<ul class="pagination"><li><span id="pagine-prev" class="btn btn-default disabled">«</span></li>';
+    //Si il y a plus ou moins de 10 pages
+    if (nbPage < 10) {
+        for (i = 0; i < nbPage; i++) {
+            if (i === 0) {
+                html += '<li><span id="pagine_page_' + (i + 1) + '" class="btn btn-default active pagine-num" data-num="' + (i + 1) + '">' + (i + 1) + '</span></li>';
+            } else {
+                html += '<li><span id="pagine_page_' + (i + 1) + '" class="btn btn-default pagine-num" data-num="' + (i + 1) + '">' + (i + 1) + '</span></li>';
+            }
+        }
+    } else {
+        html += '<li><span id="pagine_page_1" class="btn btn-default active pagine-num" data-num="1">1</span></li>';
+        html += '<li><span id="pagine_page_2" class="btn btn-default pagine-num" data-num="2">2</span></li>';
+        html += '<li><span id="pagine_page_3" class="btn btn-default pagine-num" data-num="3">3</span></li>';
+        html += '<li><span class="btn btn-default disabled">...</span></li>';
+        html += '<li><span id="pagine_page_' + (nbPage - 2) + '" class="btn btn-default pagine-num" data-num="' + (nbPage - 2) + '">' + (nbPage - 2) + '</span></li>';
+        html += '<li><span id="pagine_page_' + (nbPage - 1) + '" class="btn btn-default pagine-num" data-num="' + (nbPage - 1) + '">' + (nbPage - 1) + '</span></li>';
+        html += '<li><span id="pagine_page_' + (nbPage) + '" class="btn btn-default pagine-num" data-num="' + (nbPage) + '">' + (nbPage) + '</span></li>';
+    }
+    html += '<li><span id="pagine-next" class="btn btn-default">»</span></li></ul>';
+    $('#' + element).html(html);
+}
+
+function _change_page_pagine(newPage) {
+    //Si il y a plus ou moins de 10 pages
+    if (_pagineNbPage < 10) {
+        //Si on a pagine sans coupure on retire la class active sur le btn actuel et on le met sur le nouveau
+        $('#pagine_page_' + _paginePageActuel).removeClass('active');
+        $('#pagine_page_' + newPage).addClass('active');
+        _paginePageActuel = newPage;
+    } else {
+        var html = '<ul class="pagination"><li><span id="pagine-prev" class="btn btn-default">«</span></li>';
+        if (['1', '2', '3', '' + (_pagineNbPage - 2), '' + (_pagineNbPage - 1), '' + (_pagineNbPage)].indexOf('' + newPage) !== -1) {
+            //Code Html
+            html += '<li><span id="pagine_page_1" class="btn btn-default pagine-num" data-num="1">1</span></li>';
+            html += '<li><span id="pagine_page_2" class="btn btn-default pagine-num" data-num="2">2</span></li>';
+            html += '<li><span id="pagine_page_3" class="btn btn-default pagine-num" data-num="3">3</span></li>';
+            html += '<li><span class="btn btn-default disabled">...</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage - 2) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage - 2) + '">' + (_pagineNbPage - 2) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage - 1) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage - 1) + '">' + (_pagineNbPage - 1) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage) + '">' + (_pagineNbPage) + '</span></li>';
+        } else if (['4', '5'].indexOf('' + newPage) !== -1) {
+            //Code Html
+            html += '<li><span id="pagine_page_1" class="btn btn-default pagine-num" data-num="1">1</span></li>';
+            html += '<li><span id="pagine_page_2" class="btn btn-default pagine-num" data-num="2">2</span></li>';
+            html += '<li><span id="pagine_page_3" class="btn btn-default pagine-num" data-num="3">3</span></li>';
+            html += '<li><span id="pagine_page_4" class="btn btn-default pagine-num" data-num="4">4</span></li>';
+            html += '<li><span id="pagine_page_5" class="btn btn-default pagine-num" data-num="5">5</span></li>';
+            html += '<li><span class="btn btn-default disabled">...</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage) + '">' + (_pagineNbPage) + '</span></li>';
+        } else if (['' + (_pagineNbPage - 4), '' + (_pagineNbPage - 3)].indexOf('' + newPage) !== -1) {
+            //Code Html
+            html += '<li><span id="pagine_page_1" class="btn btn-default pagine-num" data-num="1">1</span></li>';
+            html += '<li><span class="btn btn-default disabled">...</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage - 4) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage - 4) + '">' + (_pagineNbPage - 4) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage - 3) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage - 3) + '">' + (_pagineNbPage - 3) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage - 2) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage - 2) + '">' + (_pagineNbPage - 2) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage - 1) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage - 1) + '">' + (_pagineNbPage - 1) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage) + '">' + (_pagineNbPage) + '</span></li>';
+        } else {
+            html += '<li><span id="pagine_page_1" class="btn btn-default pagine-num" data-num="1">1</span></li>';
+            html += '<li><span class="btn btn-default disabled">...</span></li>';
+            html += '<li><span id="pagine_page_' + (newPage - 1) + '" class="btn btn-default pagine-num" data-num="' + (newPage - 1) + '">' + (newPage - 1) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (newPage) + '" class="btn btn-default pagine-num" data-num="' + (newPage) + '">' + (newPage) + '</span></li>';
+            html += '<li><span id="pagine_page_' + (newPage + 1) + '" class="btn btn-default pagine-num" data-num="' + (newPage + 1) + '">' + (newPage + 1) + '</span></li>';
+            html += '<li><span class="btn btn-default disabled">...</span></li>';
+            html += '<li><span id="pagine_page_' + (_pagineNbPage) + '" class="btn btn-default pagine-num" data-num="' + (_pagineNbPage) + '">' + (_pagineNbPage) + '</span></li>';
+        }
+        html += '<li><span id="pagine-next" class="btn btn-default">»</span></li></ul>';
+        //Insertion de l'html
+        $(_pagineElement).html(html);
+        //Ajout de la class active
+        $('#pagine_page_'+ newPage).addClass('active');
+        //Mise ajour du pagine actuel
+        _paginePageActuel = newPage;
+    }
+    //si on est sur la 1er on derniere page on disabled les bon btn
+    console.log(newPage);
+    if (newPage == 1) {
+        $('#pagine-prev').addClass('disabled');
+        $('#pagine-next').removeClass('disabled');
+    } else if (newPage == _pagineNbPage) {
+        $('#pagine-next').addClass('disabled');
+        $('#pagine-prev').removeClass('disabled');
+    } else {
+        $('#pagine-prev').removeClass('disabled');
+        $('#pagine-next').removeClass('disabled');
+    }
+    _pagineCallback(newPage);
+}
 
 function animate(jqueryId, animType, callBack = function() {$(this).removeClass(); }) {
     $(jqueryId).removeClass().addClass(animType + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', callBack);
--- system/views/table.php
+++ system/views/table.php
@@ -84,26 +84,7 @@ global $_config;
     </div>
     <div class="row-fluid">
         <div class="col12" style="position: relative; margin-top: 2em; margin-bottom: 2em;">
-            <div class="pagination" style="position: absolute;top: 50%; left: 50%; transform: translate(-50%, -50%);">
-                <ul>
-                    <li><span id="prev" class="btn btn-default disabled">«</span></li>
-                    <?php
-                    for ($i = 1; $i <= $pagine; $i++) {
-                        //Si pagine inferieur a 10
-                        if ($pagine < 10) {
-                            $active = ($i === 1) ? 'active' : '';
-                            echo '<li><span id="change_page_' . $i . '" class="btn btn-default ' . $active . ' change_page" data-num="' . $i . '">' . $i . '</span></li>';
-                        } else if ($i < 4 || $i > $pagine - 3) {
-                            $active = ($i === 1) ? 'active' : '';
-                            echo '<li><span id="change_page_' . $i . '" class="btn btn-default ' . $active . ' change_page" data-num="' . $i . '">' . $i . '</span></li>';
-                        } else if ($i == 4) {
-                            echo '<li><span id="more_page" class="btn btn-default change_page disabled">...</span></li>';
-                        }
-                    }
-                    ?>
-                    <li><span id="next" class="btn btn-default">»</span></li>
-                </ul>
-            </div>
+            <div id="pagine"></div>
         </div>
     </div>
 </div>
@@ -113,76 +94,12 @@ global $_config;
 
         //Chargement des données
         updateTab();
-
-        //Numero de la page
-        var page = 1;
-        //Precedent
-        $('#prev').on('click', function () {
-            //On retire la class active
-            $('#change_page_' + page).removeClass('active');
-            //Si on est pas sur la 1er page
-            if (page > 1) {
-                page--;
-                //Si on arrive sur la 1er page on disabled le btn
-                if (page == 1) {
-                    $(this).addClass('disabled');
-                }
-                //On retire le disabled de next si il en a un
-                if ($('#next').hasClass('disabled')) {
-                    $('#next').removeClass('disabled');
-                }
-                //On change la class active pour le bon numero
-                $('#change_page_' + page).addClass('active');
-                //On appelle le script pour chercher les nouveau contenue a afficher
-                updateTab(page, $('#limit').val());
-            }
-        });
-        //Suivant
-        $('#next').on('click', function () {
-            //On retire la class active
-            $('#change_page_' + page).removeClass('active');
-            //Si on est pas sur la <?= $pagine ?>éme page
-            if (page < <?= $pagine ?>) {
-                page++;
-                //Si on arrive sur la <?= $pagine ?>éme page on disabled le btn
-                if (page == <?= $pagine ?>) {
-                    $(this).addClass('disabled');
-                }
-                //On retire le disabled de next si il en a un
-                if ($('#prev').hasClass('disabled')) {
-                    $('#prev').removeClass('disabled');
-                }
-                //On change la class active pour le bon numero
-                $('#change_page_' + page).addClass('active');
-                //On appelle le script pour chercher les nouveau contenue a afficher
-                updateTab(page, $('#limit').val());
-            }
-        });
-        //Change de numero
-        $('.change_page').on('click', function () {
-            page = $(this).attr('data-num');
-            //On retire la class active
-            $('.change_page').each(function () {
-                if ($(this).hasClass('active')) {
-                    $(this).removeClass('active');
-                }
-            });
-            //On retire ou ajoute les disabled sur prev et next selon les besoins
-            if (page == 1) {
-                $('#prev').addClass('disabled');
-            } else if ($('#prev').hasClass('disabled')) {
-                $('#prev').removeClass('disabled');
-            }
-            if (page == <?= $pagine ?>) {
-                $('#next').addClass('disabled');
-            } else if ($('#next').hasClass('disabled')) {
-                $('#next').removeClass('disabled');
-            }
-            //On le met sur le nouveau
-            $(this).addClass('active');
-            //On appelle le script pour chercher les nouveau contenue a afficher
-            updateTab(page, $('#limit').val());
-        });
+        //Pagination
+        pagine(<?= $pagine; ?>,
+                'pagine',
+                function (page) {
+                    updateTab(page);
+                });
 
         //Si la limite change
         $('#limit').on('change', function () {
