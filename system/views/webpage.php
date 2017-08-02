507955d57ce4e979063b83b0c51754cfc6b9100e
Loquicom <Loquicom@users.noreply.github.com>
2017-08-02 12:06:43 +0200

    Retrait du lien vers material icons
    Les icônes sont maintenant en locale

--- system/views/webpage.php
+++ system/views/webpage.php
@@ -7,8 +7,6 @@ global $_config;
 <!DOCTYPE html>
 <html>
     <head>
-        <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <!--Import css-->
         <link type="text/css" rel="stylesheet" href="<?=$_config['web_root']?>system/frmw/schemaUI.css"/>
         <link type="text/css" rel="stylesheet" href="<?=$_config['web_root']?>system/frmw/animate.css"/>
