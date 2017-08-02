b5387069208038e61f6380b2fa712f68acd855c4
Loquicom <Loquicom@users.noreply.github.com>
2017-08-02 11:51:00 +0200

    Ajout de la methode jquery redirectpost

--- system/frmw/script.js
+++ system/frmw/script.js
@@ -222,3 +222,31 @@ function prepare_post(sel, plus)
 
     return mypost;
 }
+
+$.extend(
+        {
+            redirectPost: function (location, args)
+            {
+                var form = $('<form></form>');
+                form.attr("method", "post");
+                form.attr("action", location);
+                if (Array.isArray(args)) {
+                    $.each(args, function(index, object){
+                        var field = $('<input></input>');
+                        field.attr("type", "hidden");
+                        field.attr("name", object.name);
+                        field.attr("value", object.value);
+                        form.append(field);
+                    });
+                } else {
+                    $.each(args, function (key, value) {
+                        var field = $('<input></input>');
+                        field.attr("type", "hidden");
+                        field.attr("name", key);
+                        field.attr("value", value);
+                        form.append(field);
+                    });
+                }
+                $(form).appendTo('body').submit();
+            }
+        });
